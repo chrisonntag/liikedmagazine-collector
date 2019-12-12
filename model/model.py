112a15de0c3c35297e32d5c1756e60978a1b40c4
Christoph Sonntag <sonntagc@fim.uni-passau.de>
2019-12-12 18:41:47 +0100

    Add basic image scraping and exend models

--- model/model.py
+++ model/model.py
@@ -1,5 +1,6 @@
 import datetime
 from peewee import *
+from settings import settings
 
 
 DATABASE = SqliteDatabase("labeled_media.db")
@@ -8,32 +9,43 @@ DATABASE = SqliteDatabase("labeled_media.db")
 class User(Model):
     user_id = IntegerField(unique=True, primary_key=True)
     username = CharField()
-    follower = IntegerField()
-    following = IntegerField()
-    parent = IntegerField()
+    follower = IntegerField(null=True, default=None)
+    following = IntegerField(null=True, default=None)
+    posts = IntegerField(null=True, default=None)
+    progress = FloatField(default=0.0)
+    parent = IntegerField(null=True, default=None)
     max_id = TextField(default="")
 
+    class Meta:
+        database = DATABASE
 
-class Image(Model):
+
+class MediaObject(Model):
     user = ForeignKeyField(User, backref='images')
-    image_id = IntegerField()
-    image_url = CharField()
-    post_url = CharField()
+    media_id = IntegerField()
+    media_high_res_url = CharField(null=True)
+    short_code = CharField()
     posted = DateTimeField()
     created = DateTimeField(default=datetime.datetime.now)
 
     quality = BooleanField(null=True, default=None)  # 1 or 0 for high or low quality
-    caption = TextField()
-    mentions = IntegerField()  # number of mentions in the caption
-    hashtags = IntegerField()  # number of hashtags
-    media_type = CharField()  # either image, video, carousel, ...
-    view_ratio = FloatField()  # views on a video / followers
-    like_ratio = FloatField()  # likes / followers
-    comment_ratio = FloatField()  # comments / followers
+    caption = TextField(null=True)
+    mentions = IntegerField(null=True)  # number of mentions in the caption
+    hashtags = IntegerField(null=True)  # number of hashtags
+    media_type = CharField(null=True)  # either image, video, carousel, ...
+    view_ratio = FloatField(null=True)  # views on a video / followers
+    like_ratio = FloatField(null=True)  # likes / followers
+    comment_ratio = FloatField(null=True)  # comments / followers
 
     class Meta:
         database = DATABASE
-        primary_key = CompositeKey('user', 'image_id')
+        primary_key = CompositeKey('user', 'media_id')
 
 
-DATABASE.create_tables([Image, User])
+def fill():
+    with DATABASE.atomic():
+        for user_id, username in settings.sources.items():
+            User.create(user_id=user_id, username=username)
+
+
+DATABASE.create_tables([MediaObject, User])
--- scraper.py
+++ scraper.py
@@ -1,6 +1,8 @@
 from time import sleep
 import logging
 from igramscraper.instagram import Instagram
+from peewee import fn
+from model.model import User, MediaObject
 from settings import settings
 from random import choice, uniform
 from helper.parser import *
@@ -17,34 +19,74 @@ instagram.with_credentials(
     scraping_user.get('password'),
     'data/'
 )
-instagram.set_account_medias_request_count(30)  # media objects in a single request
+instagram.set_account_medias_request_count(12)  # media objects in a single request
 instagram.set_user_agent(scraping_user.get('user_agent'))
 instagram.login()
 
 count = [1, 2, 3]
 wait_time = uniform(3, 5)
 
-logging.info("Wait for %d seconds" % wait_time)
-sleep(wait_time)
 
-if should_do_action(settings.like_probability):
-    tag = choice(scraping_user.get('interests'))
-    logging.info("Today we like something from #%s" % tag)
+def mimic_activity():
+    logging.info("Wait for %d seconds" % wait_time)
+    sleep(wait_time)
 
-    medias = instagram.get_medias_by_tag(tag, count=choice(count))
+    if should_do_action(settings.like_probability):
+        tag = choice(scraping_user.get('interests'))
+        logging.info("Today we like something from #%s" % tag)
 
-    for media in medias:
-        instagram.like(media.identifier)
-        print("%s liked https://www.instagram.com/p/%s" % (scraping_user.get('username'), media.short_code))
-        sleep(wait_time)
+        medias = instagram.get_medias_by_tag(tag, count=choice(count))
 
-        # Follow this user
-        if should_do_action(settings.follow_probability):
-            instagram.follow(media.owner.identifier)
-            print("%s started following user with id %s" % (scraping_user.get('username'), media.owner.identifier))
+        for m in medias:
+            instagram.like(m.identifier)
+            print("%s liked https://www.instagram.com/p/%s" % (scraping_user.get('username'), m.short_code))
+            sleep(wait_time)
+
+            # Follow this user
+            if should_do_action(settings.follow_probability):
+                instagram.follow(m.owner.identifier)
+                print("%s started following user with id %s" % (scraping_user.get('username'), m.owner.identifier))
+
 
 """
 Scrape some user data
 
-1. Get a user from the database
+1. Get a user from the database, either the user with smallest progress (breadth search) or a user with a 
+high progress (deep search). However on non parallelised bots choosing high progress would mean finishing one 
+account completely and then switching to another.
+2. Get media
+    - Update user data
+    - Populate media model
+    - Find tagged users and create a new user (with parent linked)
+    - Update user model with new max id and progress
 """
+user = User.select().where(User.progress < 0.95).order_by(User.progress.asc()).get()
+media = instagram.get_medias_by_user_id(str(user.user_id), 12, user.max_id)
+
+for m in media:
+    print(m.identifier)
+    print(m.short_code)
+    print(m.created_time)
+    print(m.type)
+    print(m.link)
+    print(m.image_high_resolution_url)
+    print(m.square_images)
+    print(m.caption)
+    print(m.is_ad)
+    print(m.likes_count)
+    print(m.comments_count)
+    print("--Owner--")
+    print(m.owner.identifier)
+    print("-------------------------------------")
+    if m.is_ad:
+        continue
+
+    image = MediaObject.create(
+        user=user,
+        media_id=m.identifier,
+        media_high_res_url=m.image_high_resolution_url,
+        short_code=m.short_code,
+        posted=m.created_time,
+        caption=m.caption,
+        media_type=m.type
+    )
