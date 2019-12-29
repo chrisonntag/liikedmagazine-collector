9527f507a95d96973418852c96d9fdff61e463d2
chrisonntag <info@chrisonntag.com>
2019-12-29 19:17:49 +0100

    Update model after download as well

--- downloader.py
+++ downloader.py
@@ -4,19 +4,32 @@ from time import sleep
 from settings import settings
 from helper.action import download_photo
 from model.model import MediaObject
+from random import uniform
 
+
+IMAGES_PER_ROUND = 12
+ROUNDS = 19
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 
-media = MediaObject.select().where(MediaObject.downloaded == False).limit(12)
+for i in range(1, ROUNDS + 1):
+    sleep(uniform(6,25))
+    media = MediaObject.select().where(MediaObject.downloaded == False).limit(IMAGES_PER_ROUND)
 
-for m in media:
-    sleep(settings.sleep_between_requests)
-    filename = str(m.user.user_id) + "." + str(m.media_id)
-    success = download_photo(m.media_high_res_url, filename)
+    for m in media:
+        sleep(0.3)
+        filename = str(m.user.user_id) + "." + str(m.media_id)
+        success = download_photo(m.media_high_res_url, filename)
 
-    if success:
-        log.info("Download of %s successful" % filename)
+        if success:
+            log.info("Download of %s successful" % filename)
+            m.downloaded = True
+            m.save()
+        else:
+            log.info("There has been an issue with %s" % filename)
+    log.info("Done %d of %d" % (i*IMAGES_PER_ROUND, IMAGES_PER_ROUND * (ROUNDS-1)))
+    log.info("-----------------------------------")
+
